4ea4495114cc2957b7fb01a0bdf91b7de21d28ba
DuaaAlk <engduaa2013@gmail.com>
2022-01-16 18:04:07 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "title"> 
+      <img className = "image"
+      src = "https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/01/1mnbcgftdrefw23.jpg"
+      alt = "Ratatiolli Resturant" />
+      <h1>Welcome to Rattatiolli</h1>
+      <p>French Cousine</p> 
     </div>
   );
 }
-
 export default App;
